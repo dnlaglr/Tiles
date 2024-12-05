a07f6d92ecf3cc5f3cf733e40635924580a6acf9
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-12-04 16:24:55 -0800

    Implement new views

--- tiles/src/main.tsx
+++ tiles/src/main.tsx
@@ -1,15 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 import Navbar from './components/Navbar'
 import Tiles from './views/tiles'
+import SignUp from './views/signup'
+import LogIn from './views/login'
+import Profile from './views/profile'
+import Activity from './views/activity'
+import Groups from './views/groups'
 
 function Layout() {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <Outlet />  {/* Render the child component */}
-    </>
+    </AuthProvider>
   )
 }
 
@@ -21,6 +27,32 @@ const router = createBrowserRouter([
       {
         path: 'map',
         element: <Tiles />
+      },
+      {
+        path: 'activity',
+        element: <Activity />
+      },
+      {
+        path: 'groups',
+        element: <Groups />
+      }
+    ]
+  },
+  {
+    path: '/auth/',
+    element: <Layout />,
+    children: [
+      {
+        path: 'signup',
+        element: <SignUp />
+      },
+      {
+        path: 'login',
+        element: <LogIn />
+      },
+      {
+        path: 'profile',
+        element: <Profile />
       }
     ]
   }
