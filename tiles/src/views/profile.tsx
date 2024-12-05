230d3515f3a8f2dea55988f829743712ab95f0c1
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-12-04 21:26:11 -0800

    Update profile features

--- tiles/src/views/profile.tsx
+++ tiles/src/views/profile.tsx
@@ -8,6 +8,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
 import profilePlaceholder from '../assets/profile/profile-placeholder.jpg'
 import getGroups from '../util/groups/getGroups';
 import getUser from '../util/users/getUser';
+import deleteUserData from '../util/users/deleteUser';
 
 export default function Profile() {
   const auth = useAuth();
@@ -20,34 +21,51 @@ export default function Profile() {
     navigate('/auth/login');
   }
 
+  function deleteUser() {
+    if (auth?.currentUser) {
+      deleteUserData(auth, auth?.currentUser?.uid);
+      navigate('/auth/signup');
+    }
+  }
+
   useEffect(() => {
     if (auth?.getUser() == null) {
       navigate('/auth/login');
     }
 
     async function getUserDoc() {
-      const userQuery = await getUser(auth?.getUser()?.uid);
-      setUser(userQuery);
+      if (auth?.currentUser) {
+        const userQuery = await getUser(auth.currentUser.uid);
+        setUser(userQuery);
+      }
     }
 
     async function getGroupDocs() {
-      const groupQuery = await getGroups(auth?.getUser()?.uid);
-      setGroups(groupQuery);
+      if (auth?.currentUser) {
+        const groupQuery = await getGroups(auth.currentUser.uid);
+        setGroups(groupQuery);
+      }
     }
 
     getUserDoc();
     getGroupDocs();
   }, [])
 
+  console.log(user)
 
   return (
     <div className='flex justify-center items-center w-screen h-[calc(100vh_-_56px)]'>
       <div className='flex flex-row justify-between items-center w-[50%] h-[80%] rounded-md border border-gray-100 shadow-md p-12'>
-        <div className='flex flex-col justify-evenly items-center w-1/3 h-full'>
+        <div className='flex flex-col justify-between items-center w-1/3 h-full border-r'>
           {/* Profile Picture, Username, Join Date, Membership Status */}
-          <img className='w-[200px] h-[200px] border border-gray-100 rounded-md' src={profilePlaceholder}></img>
-          <h1>{user?.displayName == null ? '' : user?.displayName}</h1>
-          <button onClick={logOutUser} className='w-[90%] h-10 bg-black hover:bg-white text-white hover:text-black rounded-lg border-2 border-black mt-4'>Log Out</button>
+          <div className='flex flex-col justify-evenly items-center pt-16'>
+            <img className='w-[200px] h-[200px] border border-gray-100 rounded-md' src={profilePlaceholder}></img>
+            <h1 className='pt-4'>{user?.displayName == null ? '' : user?.displayName}</h1>
+          </div>
+          <div className='flex flex-col justify-evenly items-center w-[80%] pb-24'>
+            <button onClick={logOutUser} className='w-[90%] h-10 bg-black hover:bg-white text-white hover:text-black rounded-lg border-2 border-black mt-4'>Log Out</button>
+            <button onClick={deleteUser} className='w-[90%] h-10 bg-red-700 hover:bg-white text-white hover:text-red-700 rounded-lg border-2 border-red-700 mt-4'>Delete Account</button>
+          </div>
         </div>
 
         <div className='flex flex-col justify-start items-start w-2/3 h-full p-6 pr-0'>
@@ -69,7 +87,7 @@ export default function Profile() {
 
               }
               {/* Join Group Button */}
-              <div className='flex flex-row justify-between items-center w-full px-4 py-2 mb-2 rounded-md bg-gray-50 cursor-pointer'>
+              <div onClick={() => navigate('/groups')} className='flex flex-row justify-between items-center w-full px-4 py-2 mb-2 rounded-md bg-gray-50 cursor-pointer'>
                 <h2 className='text-base text-gray-900 font-medium'>Join a Group</h2>
                 <FontAwesomeIcon icon={faPlus} />
               </div>
