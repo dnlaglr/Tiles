fe8d3473360536b5d5865525c1da799d06d1d797
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-12-05 17:04:25 -0800

    Create function to fetch member docs

---
+++ tiles/src/util/map/getTiles.ts
@@ -0,0 +1,21 @@
+import { collection, DocumentData, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../firebase';
+
+export default async function getTiles(userID: string) : Promise<DocumentData> {
+  try {
+    const groupsRef = collection(db, "groups");
+    const groupsQuery = query(groupsRef, where("memberIDs", "array-contains", userID));
+    const groupSnapshot = await getDocs(groupsQuery);
+    const groupDocs = groupSnapshot.docs;
+
+    const membersRef = collection(groupDocs[0].ref, "members");
+    const membersSnapshot = await getDocs(membersRef);
+    const membersDocs = membersSnapshot.docs.map((doc) => doc.data());
+    
+    console.log(membersDocs);
+    return membersDocs;
+
+  } catch (err) {
+    console.error("[ ERROR ] Couldn't fetch tiles: ", err);
+  }
+}
